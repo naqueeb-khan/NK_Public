0e05034b30d8c948af1e0d5a58ae3310a4bb6ba2
Naqueeb Khan <naqueeb.khan.843@gmail.com>
2026-02-28 17:00:59 +0530

    added get and post method

--- rts/src/main/java/com/rts/controllers/UserCtl.java
+++ rts/src/main/java/com/rts/controllers/UserCtl.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping(path = Constant.USERCTL)
 public class UserCtl extends BaseCtl {
     @GetMapping("/getUser")
-    @ResponseBody
     public ResponseEntity<ResponseData<User>> getMethodName() {
         System.out.println("user controller");
         User user=new User();
@@ -30,7 +29,7 @@ public class UserCtl extends BaseCtl {
         response.setList(null);  
         return  ResponseEntity.ok(response);
     }
-    @PostMapping("/addOrUpdateUser")
+    @PostMapping("/postUser")
     public ResponseData<User> postMethodName(@RequestBody ResponseData<User> req) {
         
         System.out.println("user controller");
